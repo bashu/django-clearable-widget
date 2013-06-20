ece63f018d14028471f02c22379ae183aca36a6c
Basil Shubin <basil.shubin@gmail.com>
2013-06-20 12:15:03 +0700

    ops! fix imports

--- clearable_widget/widgets.py
+++ clearable_widget/widgets.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django import forms
-from django.utils.safestring import mark_safe
+from django.template.loader import render_to_string
 from django.contrib.staticfiles.storage import staticfiles_storage
 
 __all__ = ['ClearableInput']
@@ -24,4 +24,4 @@ class ClearableInput(MediaMixin, forms.TextInput):
         if value is None: value = ''
 
         return render_to_string(self.template_name, {
-                'widget': super(ClearableInput, self).render(name, value, attrs), 'name': name}
+                'widget': super(ClearableInput, self).render(name, value, attrs), 'name': name})
