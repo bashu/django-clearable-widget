ba4a6a127d80c10b6ba7be578fb27f76bff64ef8
Basil Shubin <basil.shubin@gmail.com>
2013-08-05 17:01:20 +0700

    improve tests

--- clearable_widget/tests/test_widgets.py
+++ clearable_widget/tests/test_widgets.py
@@ -9,27 +9,38 @@ from ..widgets import ClearableInput
 
 class ClearableInputTest(TestCase):
 
-    def test_jinja_template(self):
-        old_USE_JINJA = getattr(settings, 'USE_JINJA', False)
-        settings.USE_JINJA = True
-        self.field = forms.CharField(required=False, widget=ClearableInput)
-        response = self.field.widget.render('value', 'test', {'id': 'id_field'})
-        self.assertTrue('clear-holder' in response and 'test' in response)
-
-        settings.USE_JINJA = old_USE_JINJA
-
-    def test_django_template(self):
-        old_USE_JINJA = getattr(settings, 'USE_JINJA', False)
-        settings.USE_JINJA = False
-
-        self.field = forms.CharField(required=False, widget=ClearableInput)
-        response = self.field.widget.render('value', 'test', {'id': 'id_field'})
-        self.assertTrue('clear-holder' in response and 'test' in response)
-
-        settings.USE_JINJA = old_USE_JINJA
-
-    def test_empty(self):
+    def test_default(self):
         self.field = forms.CharField(required=False, widget=ClearableInput)
 
         response = self.field.widget.render('value', None, {'id': 'id_field'})
         self.assertTrue('clear-holder' in response and not "None" in response)
+
+
+class ClearableInputDjangoTest(TestCase):
+
+    def setUp(self):
+        self.old_USE_JINJA = getattr(settings, 'USE_JINJA', False)
+        settings.USE_JINJA = False
+
+    def tearDown(self):
+        settings.USE_JINJA = self.old_USE_JINJA
+
+    def test_template(self):
+        self.field = forms.CharField(required=False, widget=ClearableInput)
+        response = self.field.widget.render('value', 'test', {'id': 'id_field'})
+        self.assertTrue('clear-holder' in response and 'test' in response)
+
+
+class ClearableInputJinjaTest(TestCase):
+
+    def setUp(self):
+        self.old_USE_JINJA = getattr(settings, 'USE_JINJA', False)
+        settings.USE_JINJA = True
+
+    def tearDown(self):
+        settings.USE_JINJA = self.old_USE_JINJA
+
+    def test_template(self):
+        self.field = forms.CharField(required=False, widget=ClearableInput)
+        response = self.field.widget.render('value', 'test', {'id': 'id_field'})
+        self.assertTrue('clear-holder' in response and 'test' in response)
