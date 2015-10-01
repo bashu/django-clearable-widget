1a512e9663e62f1ebafc65bdc0ccb9f620c0937b
Basil Shubin <basil.shubin@gmail.com>
2015-10-01 16:41:46 +0600

    improved test suite

--- setup.py
+++ setup.py
@@ -1,24 +1,82 @@
+#!/usr/bin/env python
+
 import os
+import re
+import sys
+import codecs
+import subprocess
+
 from setuptools import setup, find_packages
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+from setuptools.command.test import test as TestCommand
 
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+class TestRunner(TestCommand):
+    user_options = []
+
+    def run(self):
+        raise SystemExit(subprocess.call([sys.executable, 'runtests.py']))
+
+
+# When creating the sdist, make sure the django.mo file also exists:
+if 'sdist' in sys.argv:
+    os.chdir('clearable_widget')
+    try:
+        from django.core.management.commands.compilemessages import Command
+        command = Command()
+        command.execute(stdout=sys.stderr, verbosity=1)
+    except ImportError:
+        # < Django 1.7
+        from django.core.management.commands.compilemessages import compile_messages
+        compile_messages(sys.stderr)
+    finally:
+        os.chdir('..')
+
+
+def read(*parts):
+    file_path = os.path.join(os.path.dirname(__file__), *parts)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+def find_version(*parts):
+    version_file = read(*parts)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return str(version_match.group(1))
+    raise RuntimeError("Unable to find version string.")
+
 
 setup(
     name='django-clearable-widget',
-    version='0.1.4',
-    packages=find_packages(exclude=['example']),
-    include_package_data=True,
+    version=find_version('clearable_widget', '__init__.py'),
     license='BSD License',
+
+    install_requires=[
+        'django>=1.4.2',
+    ],
+    requires=[
+        'Django (>=1.4.2)',
+    ],
+
     description='Custom widget to add a (x) clear button to your input fields',
-    long_description=README,
-    url='http://github.com/bashu/django-clearable-widget',
+    long_description=read('README.rst'),
+
     author='Basil Shubin',
     author_email='basil.shubin@gmail.com',
-    install_requires=[
+
+    url='http://github.com/bashu/django-clearable-widget',
+    download_url='https://github.com/bashu/django-clearable-widget/zipball/master',
+
+    packages=find_packages(exclude=('example*', '*.tests*')),
+    include_package_data=True,
+
+    tests_require=[
     ],
+    cmdclass={
+        'test': TestRunner,
+    },
+
+    zip_safe=False,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
@@ -34,5 +92,4 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
-    zip_safe=False,
 )
